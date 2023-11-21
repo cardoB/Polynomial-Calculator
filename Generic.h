9c03b6ef87cc47998e7da4d4b3498d5535acae8e
cardoB <69881375+cardoB@users.noreply.github.com>
2023-11-21 15:01:03 -0500

    Create Generic.h

---
+++ Generic.h
@@ -0,0 +1,17 @@
+//
+//  Generic.h
+//  Polynomial Calculator
+//
+//  Created by Riccardo Belletta on 11/21/23.
+//
+
+#ifndef Generic_h
+#define Generic_h
+
+void welcome();
+
+int menu();
+
+Polynomial getPolynomial(char variable);
+
+#endif /* Generic_h */
